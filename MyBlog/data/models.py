47f2e5777c6ca008199ad1ba694eb8f0654979a6
Shervin Ara <shervin_ara@yahoo.com>
2013-02-17 18:10:22 +0330

    style improved

--- MyBlog/data/models.py
+++ MyBlog/data/models.py
@@ -4,9 +4,10 @@ from django.db import models
 # Create your models here.
 class context(models.Model):
 	Name=models.CharField(max_length=30)
-	Email=models.EmailField()
 	Post=models.TextField()
 	Subject=models.CharField(max_length=30)
+	Date=models.DateField(auto_now=True, auto_now_add=True,)
+	Time=models.TimeField(auto_now=True, auto_now_add=True,)
 	
 	def __unicode__(self):
 		return "%s - %s - %s" % (self.Name,self.Post,self.Subject) 
