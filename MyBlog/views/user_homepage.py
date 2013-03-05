a3d5f21f8533f297f9805d3a7f3be191ff604633
Sameer Rahmani <lxsameer@gnu.org>
2013-03-05 21:26:47 +0330

    Some bug fixed and source tree cleaned up

--- MyBlog/views/user_homepage.py
+++ MyBlog/views/user_homepage.py
@@ -1,23 +1,25 @@
 #coding:    utf8
-import  datetime
+import datetime
 
-from    django.http import  HttpResponse    ,   Http404 ,   HttpResponseRedirect
-from    django.shortcuts    import  render_to_response  as  rr
-from    django.contrib.auth.models  import  User
-from    django.contrib.auth import  authenticate,   login
-from    django.template import  RequestContext
+from django.http import  HttpResponse    ,   Http404 ,   HttpResponseRedirect
+from django.shortcuts    import  render_to_response  as  rr
+from django.contrib.auth.models  import  User
+from django.contrib.auth import  authenticate,   login
+from django.template import  RequestContext
+from django.contrib.auth.decorators import login_required
 
-from    MyBlog.data.models  import  context
-from    MyBlog.data.models  import  comment
-from    MyBlog.forms    import  AuthForm
+from MyBlog.data.models  import  context
+from MyBlog.data.models  import  comment
+from MyBlog.forms    import  AuthForm
 
 
-now =   datetime.datetime.now()
+now = datetime.datetime.now()
 
-def user_homepage(request,user):    
-    contexts    =   context.user_post(user)
+@login_required
+def user_homepage(request,user):
+    contexts = context.user_post(request.user)
     return  rr('home.html',
-        {'now':now,
-        'contexts':contexts,
-        'user':user},
-    context_instance=RequestContext(request))
+               {'now':now,
+                'contexts':contexts,
+                'user':user},
+                context_instance=RequestContext(request))
